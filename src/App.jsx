a479365b6d37dde2dac3b69032f4091467624e09
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-09-06 00:01:26 -0500

    useFilters

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import { useState } from 'react'
 
+import { useFilters } from './hooks/useFilters'
+
 import { Products } from './components/Products'
 import { Header } from './components/Header'
 
 import { products as initialProducts } from './mocks/products.json'
 
 function App() {
-	const [products, setProducts] = useState(initialProducts)
-	const [filters, setFilters] = useState({
-		category: 'all',
-		minPrice: 0,
-	})
-
-	const filterProducts = (products) => {
-		return products.filter((product) => {
-			return (
-				product.price >= filters.minPrice &&
-				(filters.category === 'all' || product.category === filters.category)
-			)
-		})
-	}
+	const [products] = useState(initialProducts)
+	const { filterProducts, setFilters } = useFilters()
 
 	const filteredProducts = filterProducts(products)
 
